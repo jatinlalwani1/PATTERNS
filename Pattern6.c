1f1e9578a2c263e1340f962f403408a63412fc4f
jatinlalwani1 <166413202+jatinlalwani1@users.noreply.github.com>
2024-04-14 21:08:08 +0530

    Add files via upload
    Pattern number 6 Star pattern

---
+++ Pattern6.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+int main(){
+    
+    for (int i=1;i<=5;i++){
+        int k=1;
+        for (int j=1;j<=9;j++){
+           if (j>=6-i && j<=4+i && k){ 
+            printf("*");
+            k=0;
+           }
+           else{
+            printf(" ");
+            k=1;
+           }
+        }
+        printf("\n");
+    
+    }
+return 0;
+}
