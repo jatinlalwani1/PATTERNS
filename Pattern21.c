f5a59d4916a3d7a7c92b723fcd3c68ac37e223d8
jatinlalwani1 <166413202+jatinlalwani1@users.noreply.github.com>
2024-04-18 22:37:25 +0530

    Add files via upload
    Pattern number 21 Star pattern

---
+++ Pattern21.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+int main(){
+    
+    for (int i=1;i<=4;i++){
+        for (int j=1;j<=8;j++){
+           if (j>=5-i && j<=9-i){ 
+            printf("*");
+           }
+           else{
+            printf(" ");
+           }
+        }
+        printf("\n");
+    
+    }
+return 0;
+}
