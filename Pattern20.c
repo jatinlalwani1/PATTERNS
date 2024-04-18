c2ef44c89f33f439fea3d924c10abea70cd2d78f
jatinlalwani1 <166413202+jatinlalwani1@users.noreply.github.com>
2024-04-18 22:33:54 +0530

    Add files via upload
    Pattern number 20 Alphanumeric pattern

---
+++ Pattern20.c
@@ -0,0 +1,23 @@
+# include<stdio.h>
+int main(){
+    
+    char k;
+    for (int i=1;i<=4;i++){
+        k='A';
+        for (int j=1;j<=8;j++){
+           if (j>=5-i && j<=4+i){ 
+            if (j==5){
+                k='1';
+            }
+            printf("%c",k);
+            k++;
+           }
+           else{
+            printf(" ");
+           }
+        }
+        printf("\n");
+    
+    }
+return 0;
+}
